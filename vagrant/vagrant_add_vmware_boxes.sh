fd5b3142d984dd0eb2b8f07a13454833f7bfd3e6
Jamie McMurray <jamie.mcmurray@ITG02327.local>
2020-11-02 20:29:33 -0800

    Fix filter for linux

--- vagrant/vagrant_add_vmware_boxes.sh
+++ vagrant/vagrant_add_vmware_boxes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting vagrant update..."
 for box in $(ls -f "../windows/" | grep windows_); do vagrant box add ../windows/$box/builds/vmware-${box}.box --name vmware-${box} --force; done
-for box in $(ls -f "../linux/" | grep -v "."); do vagrant box add ../linux/$box/builds/vmware-${box}.box --name vmware-${box} --force; done
+for box in $(ls -f "../linux/" | grep -v "\."); do vagrant box add ../linux/$box/builds/vmware-${box}.box --name vmware-${box} --force; done
 echo "Waiting for all background jobs to complete..."
 wait
 
