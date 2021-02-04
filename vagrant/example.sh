45725ae20fdaddcf87bbef690dcfc6999c0a96a6
scalatar <jamiefrasermcmurray@gmail.com>
2021-02-04 10:10:17 -0800

    Update example

--- vagrant/example.sh
+++ vagrant/example.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-vagrant destroy -f && vagrant up && scp ../ansible/inventory/lab.yml vagrant@172.21.0.30:~ && scp ../../terraform-lab-build/templates/ansible_awx_docker.sh vagrant@172.21.0.30:~ && ssh vagrant@172.21.0.30 -C "chmod 755 ~/ansible_awx_docker.sh && ~/ansible_awx_docker.sh && cd /opt/repo/splunk-lab/ansible && ansible-playbook -i ~/lab.yml playbooks/build-env.yml"
+vagrant destroy -f
+vagrant up
+scp ../ansible/inventory/lab.yml vagrant@172.21.0.30:~
+scp ../../terraform-lab-build/templates/ansible_awx_docker.sh vagrant@172.21.0.30:~ 
+ssh vagrant@172.21.0.30 -C "chmod 755 ~/ansible_awx_docker.sh"
+ssh vagrant@172.21.0.30 -C "~/ansible_awx_docker.sh"
+ssh vagrant@172.21.0.30 -C "cd /opt/repo/splunk-lab/ansible; ansible-playbook -i ~/lab.yml playbooks/build-env.yml"
