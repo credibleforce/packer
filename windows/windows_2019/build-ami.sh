7f6fb8d829cfb7ff245bd1fa5988f45f2914c8ce
scalatar <jamiefrasermcmurray@gmail.com>
2021-02-17 09:57:16 -0800

    Add win19 ami build

---
+++ windows/windows_2019/build-ami.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PACKER_LOG=1 packer build \
+  --var iso_url=iso/windows_2019.iso \
+  --var iso_checksum=sha256:549bca46c055157291be6c22a3aaaed8330e78ef4382c99ee82c896426a1cee1 \
+  box-config-ami.json
