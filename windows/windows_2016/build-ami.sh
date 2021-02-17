5a3eb13933182dfb56dffb6654ac714850fcf833
scalatar <jamiefrasermcmurray@gmail.com>
2021-02-17 09:23:45 -0800

    Add win16 build ami

---
+++ windows/windows_2016/build-ami.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PACKER_LOG=1 packer build  \
+    --var iso_url=iso/windows_2016.iso \
+    --var iso_checksum=sha256:1ce702a578a3cb1ac3d14873980838590f06d5b7101c5daaccbac9d73f1fb50f \
+    box-config-ami.json
