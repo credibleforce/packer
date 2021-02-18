cc2bf31657fbdf5497f976abaec94ab42850f40d
scalatar <jamiefrasermcmurray@gmail.com>
2021-02-17 21:39:32 -0800

    Update setup-complete.bat

---
+++ windows/shared/scripts/setup-complete.bat
@@ -0,0 +1,2 @@
+net stop tiledatamodelsvc
+call c:\windows\system32\shutdown.exe /s /t 10 /f /d p:4:1 /c "Packer Shutdown"
--- windows/shared/scripts/shutdown.bat
+++
@@ -1,3 +0,0 @@
-echo "Shutting down" > "%temp%\shutdown.txt"
-net stop tiledatamodelsvc > /nul 2>&1
-c:\windows\system32\shutdown.exe /s /t 10 /f /d p:4:1 /c Packer_Provisioning_Shutdown
