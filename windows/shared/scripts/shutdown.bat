d7c3b9d3c015cf0d64337dacf89d9239314c0726
scalatar <jamiefrasermcmurray@gmail.com>
2021-02-17 20:59:37 -0800

    Update shutdown

--- windows/shared/scripts/shutdown.bat
+++ windows/shared/scripts/shutdown.bat
@@ -1,2 +1,3 @@
+echo "Shutting down" > "%temp%\shutdown.txt"
 net stop tiledatamodelsvc > /nul 2>&1
-shutdown /s /t 10 /f /d p:4:1 /c Packer_Provisioning_Shutdown
+c:\windows\system32\shutdown.exe /s /t 10 /f /d p:4:1 /c Packer_Provisioning_Shutdown
