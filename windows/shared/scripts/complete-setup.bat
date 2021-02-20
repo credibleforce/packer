0afd9c63d6b525083cc856796b4614e79580c5f0
scalatar <jamiefrasermcmurray@gmail.com>
2021-02-20 12:23:22 -0800

    Update ami base

--- windows/shared/scripts/complete-setup.bat
+++ windows/shared/scripts/complete-setup.bat
@@ -1,2 +1,7 @@
 net stop tiledatamodelsvc
-call c:\windows\system32\shutdown.exe /s /t 10 /f /d p:4:1 /c "Packer Shutdown"
+%SystemRoot%\System32\reg.exe DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /f
+%SystemRoot%\System32\reg.exe DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /f
+REM set MY_SESSION_ID=unknown
+REM for /f "tokens=3-4" %%a in ('query session %username%') do @if "%%b"=="Active" set MY_SESSION_ID=%%a
+REM logoff %MY_SESSION_ID%
+start %SystemRoot%\System32\shutdown /s /f /t 1800 /c "Packer shutdown"
