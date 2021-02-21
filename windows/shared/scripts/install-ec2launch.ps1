a69ecd3fdf7c2355388bfec23fd63babd0253ae1
scalatar <jamiefrasermcmurray@gmail.com>
2021-02-21 09:38:37 -0800

    Update EC2 prep

--- windows/shared/scripts/install-ec2launch.ps1
+++ windows/shared/scripts/install-ec2launch.ps1
@@ -1,10 +1,18 @@
 $EC2LaunchZip = 'https://s3.amazonaws.com/ec2-downloads-windows/EC2Launch/latest/EC2-Windows-Launch.zip'
 $OutPath = "C:\ProgramData\Amazon\EC2-Windows\EC2-Windows-Launch.zip"
-$LPath = 'C:\ProgramData\Amazon\EC2-Windows\Launch'
-New-Item -ItemType Directory -Force -Path $LPath
+$EC2LaunchInstall = 'https://s3.amazonaws.com/ec2-downloads-windows/EC2Launch/latest/install.ps1'
+$OutInstallPath = "C:\ProgramData\Amazon\EC2-Windows\install.ps1"
+
+
+# $LPath = 'C:\ProgramData\Amazon\EC2-Windows\Launch'
+# New-Item -ItemType Directory -Force -Path $LPath
+
 Invoke-WebRequest -Uri $EC2LaunchZip -OutFile $OutPath
-Expand-Archive -Path $OutPath -DestinationPath $LPath
-Remove-Item $OutPath
+Invoke-WebRequest -Uri $EC2LaunchInstall -OutFile $OutInstallPath
+& $EC2LaunchInstall
+
+# Expand-Archive -Path $OutPath -DestinationPath $LPath
+# Remove-Item $OutPath
 
 # prepare the custom unattend if it exists
 if(test-path "A:\AWS_Unattend.xml"){
