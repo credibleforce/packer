febd10f52e1dbe40c3a91d53800afda87f3c0ed0
scalatar <jamiefrasermcmurray@gmail.com>
2021-03-04 16:31:24 -0800

    Update base

--- windows/shared/scripts/sysprep.bat
+++ windows/shared/scripts/sysprep.bat
@@ -1,7 +1,7 @@
 net stop tiledatamodelsvc > /nul 2>&1
 if exist a:\unattend.xml (
-  c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /unattend:a:\unattend.xml
+  c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /quiet /unattend:a:\unattend.xml
 ) else (
   del /F \Windows\System32\Sysprep\unattend.xml
-  c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /shutdown
+  c:\windows\system32\sysprep\sysprep.exe /generalize /quiet /oobe
 )
